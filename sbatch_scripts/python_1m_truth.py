89144c59be67a38a6d843a14370b0166bf913b7c
Anna Jaruga <ajaruga@caltech.edu>
2020-01-16 15:09:58 -0800

    ...

--- sbatch_scripts/python_1m_truth.py
+++ sbatch_scripts/python_1m_truth.py
@@ -1,6 +1,6 @@
 import os
 
-outdir = "/home/ajaruga/clones/UWLCM/build/out_test_blk_1m_truth_long"
+outdir = "/home/ajaruga/clones/UWLCM/build/out_test_blk_1m_spinup_01"
 
 case = "dycoms_rf02"
 nx = "129"
@@ -8,13 +8,12 @@ ny = "0"
 nz = "301"
 dt = "1"
 spinup = "3600"
-nt = "25200"
+nt = "4500"
 micro = "blk_1m"
 outfreq = "900"
 backend = "serial"
 
-
-cmd = "OMP_NUM_THREADS=32 /home/ajaruga/clones/UWLCM/build/src/bicycles"+\
+cmd = "OMP_NUM_THREADS=1 /home/ajaruga/clones/UWLCM/build/src/bicycles"+\
       " --outdir="+outdir+" --case="+case+\
       " --nx="+nx+" --ny=0 --nz="+nz+" --dt="+dt+" --spinup="+spinup+\
       " --nt="+nt+" --micro="+micro+" --outfreq="+outfreq+\
--- sbatch_scripts/run_1m_truth.sh
+++ sbatch_scripts/run_1m_truth.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1                   # Use one node
 #SBATCH --ntasks=1                  # Number of CPUs
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32          # number of OpenMP threads?
+#SBATCH --cpus-per-task=1           # number of OpenMP threads?
 #SBATCH --mem-per-cpu=1G
 #SBATCH --partition=any
 
